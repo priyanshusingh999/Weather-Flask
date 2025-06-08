22b206affb7bbfe93bfd9d2422162d15b97214cb
Priyanshu Singh Rajput <singpriyanshu24+github@gmail.com>
2025-06-08 06:53:16 +0530

    add port

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def home():
     return render_template('index.html', city=city, weather=weather)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=False, host='0.0.0.0', port=8000)
